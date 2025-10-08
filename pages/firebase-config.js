2833fb2e7a25d464017cbfe5d08037b2ab4ff5d3
Sheniya <niyajamdev@gmail.com>
2025-10-08 19:14:47 +0530

    updated

--- pages/firebase-config.js
+++ pages/firebase-config.js
@@ -1,9 +1,8 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getDatabase, ref, set, get, push, onValue } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBLSW2MSs9u7amgxiOUzLehjgUJMv_Ci4E",
   authDomain: "tn-bus-tracker-1b4d8.firebaseapp.com",
@@ -18,17 +17,9 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
-// Convert email to Firebase-safe key
-function keyFromEmail(email){
+// Helper to make Firebase keys safe
+function keyFromEmail(email) {
   return email.replace(/\./g,'_');
 }
 
-// Logout helper
-async function logout() {
-  const user = auth.currentUser;
-  if(user) await set(ref(db, `sessions/${user.uid}`), null);
-  await signOut(auth);
-  window.location.href='index.html';
-}
-
-export { db, ref, set, get, push, onValue, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, keyFromEmail, logout };
+export { db, ref, set, auth, createUserWithEmailAndPassword, keyFromEmail };
